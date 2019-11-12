4883240f472a1453130c732bf745ca42e825614f
Khadgar <blyashuk@list.ru>
2019-11-12 11:52:35 +0300

    Update Manifest, Settings activity. Add Change Pin code Activity

---
+++ app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/activities/ChangePinCode.kt
@@ -0,0 +1,13 @@
+package ru.vadimbliashuk.secondsaveyourpassword.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.vadimbliashuk.secondsaveyourpassword.R
+
+class ChangePinCode : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pin_code)
+    }
+}
--- app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/activities/SettingsActivity.kt
+++ app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/activities/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package ru.vadimbliashuk.secondsaveyourpassword.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_setting.*
 import ru.vadimbliashuk.secondsaveyourpassword.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        tv_settings_update_pin.setOnClickListener {
+            val intent = Intent(this, ChangePinCode::class.java)
+            startActivity(intent)
+        }
     }
 }
