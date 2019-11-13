7a53b9a5fec0849245bf46a5c7022f0a71bffcbd
Khadgar <blyashuk@list.ru>
2019-11-13 13:00:48 +0300

    Add update Activity

--- app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/activities/AllLoginsActivity.kt
+++ app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/activities/AllLoginsActivity.kt
@@ -14,6 +14,7 @@ import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_all_logins.*
+import kotlinx.android.synthetic.main.recycler_view_item.view.*
 import ru.vadimbliashuk.secondsaveyourpassword.R
 import ru.vadimbliashuk.secondsaveyourpassword.adapter.UserListAdapter
 import ru.vadimbliashuk.secondsaveyourpassword.data.UserViewModel
@@ -77,7 +78,7 @@ class AllLoginsActivity : AppCompatActivity(),
     override fun onItemLongClick(view: View, position: Int) {
         Toast.makeText(
             this,
-            "long",
+            view.tv_website_rv_item.text.toString(),
             Toast.LENGTH_LONG
         ).show()
 
