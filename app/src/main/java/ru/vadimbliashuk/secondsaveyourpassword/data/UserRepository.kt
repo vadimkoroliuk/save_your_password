35610fb360b947fae59a751443c0eb8947947c29
Khadgar <blyashuk@list.ru>
2020-01-18 00:18:47 +0300

    Add description(viewModel) for all database.

--- app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/data/UserRepository.kt
+++ app/src/main/java/ru/vadimbliashuk/secondsaveyourpassword/data/UserRepository.kt
@@ -18,3 +18,26 @@ class UserRepository(private val userDao: UserDao) {
         userDao.delete(user)
     }
 }
+
+//class UserViewModel(application: Application) : AndroidViewModel(application) {
+//    private val repository: UserRepository
+//    val allUsers: LiveData<List<UserEntity>>
+//
+//    init {
+//        val userDao = UserDatabase.getInstance(application).userDao()
+//        repository = UserRepository(userDao)
+//        allUsers = repository.allUsers
+//    }
+//
+//    fun insert(user: UserEntity) = viewModelScope.launch {
+//        repository.insert(user)
+//    }
+//
+//    fun update(user: UserEntity) = viewModelScope.launch {
+//        repository.update(user)
+//    }
+//
+//    fun delete(user: UserEntity) = viewModelScope.launch {
+//        repository.delete(user)
+//    }
+//}
